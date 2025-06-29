83338f4c3b1a8300a672c8b0b33f2a8087ad5020
Vanessa <vanessamedina2281@gmail.com>
2025-06-28 18:50:27 -0700

    finished ttt game logic and ui

--- index.js
+++ index.js
@@ -1,21 +1,98 @@
-document.getElementById("startbtn").addEventListener("click", function ( ) {
-    this.textContent = "Clear";
-    clearTable();
-    
-});
+const cells = document.querySelectorAll(".cell");
+const status = document.querySelector("#statusTxt");
+const restartBtn = document.querySelector("#restartBtn");
+const winConditions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
 
+let options = ["","","","", "", "", "", "", ""];
+let currentPlayer = "X";
+let running = false;
 
-function clearTable () {
-    const cells = document.querySelectorAll(".table_content");
+initializeGame();
 
+function initializeGame(){
+    cells.forEach(cell => cell.addEventListener("click", cellClicked))
+    restartBtn.addEventListener("click", restartGame);
+    status.textContent = `${currentPlayer}'s turn`;
+    running = true;
+}
+
+function cellClicked(){
+    const cellIndex = this.getAttribute("cellIndex");
+
+    if(options[cellIndex] != "" || !running){
+        return;
+    }
+
+    updateCell(this,cellIndex);
+    checkWinner();
+}
+
+function updateCell(cell, index){
+    options[index]=currentPlayer;
+    cell.textContent =  currentPlayer;
+}
+
+function changePlayer(){
+    currentPlayer = (currentPlayer == "X") ? "O" : "X";
+    statusTxt.textContent = `${currentPlayer}'s turn`;
+}
+
+function checkWinner(){
+    let roundWon = false;
+    let winningCombo = [];
+
+    for(let i = 0; i< winConditions.length; i++){
+        const [a, b, c] = winConditions[i];
+        const cellA = options[a];
+        const cellB = options[b];
+        const cellC = options[c];
+
+        if(cellA == "" || cellB == ""||cellC == ""){
+            continue;
+        }
+        if(cellA == cellB && cellB == cellC){
+            roundWon = true;
+            winningCombo = [a, b, c];
+            break;
+        }
+    }
+
+    if(roundWon){
+        status.textContent = `${currentPlayer} wins!`;
+        running = false;
+
+        winningCombo.forEach(index => {
+            cells[index].style.color = "red";
+        });
+    }
+    else if(!options.includes("")){
+        status.textContent = `Draw!`;
+        running = false;
+    }
+    else{
+        changePlayer();
+    }
+
+    restartBtn.textContent=`Restart`;
+}
+
+function restartGame(){
+    currentPlayer = "X";
+    options = ["","","","", "", "", "", "", ""];
+    status.textContent = `${currentPlayer}'s turn`;
     cells.forEach(cell => {
-        cell.value = "";
+        cell.textContent = "";
+        cell.style.color = "black"; // reset color
     });
+    running =true;
+
 }
-
-
-function compMoves () {
-    
-}
-
-
